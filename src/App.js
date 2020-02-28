0dbac1f702217e54ffb20e3448d5e382af267f4f
kiran <gandikotakiran99@gmail.com>
2020-02-28 10:13:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div >
+      Lord Ram
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
